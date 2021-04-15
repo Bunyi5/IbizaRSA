0aa6a692ca41ecd80f72535eaf691a9893385281
Bunyi5 <bbakos262@gmail.com>
2021-04-16 00:22:43 +0200

    RSA done

---
+++ src/RSA.java
@@ -0,0 +1,87 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RSA {
+
+    private final BigInteger p, q, n, phiN, e, d;
+
+    public RSA() {
+        this.p = generateRandomPrime();
+        this.q = generateRandomPrime();
+        this.n = p.multiply(q);
+        this.phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        this.e = chooseE();
+        this.d = Mod.modInverse(e, phiN);
+    }
+
+    public RSA(BigInteger p, BigInteger q, BigInteger e) {
+        this.p = p;
+        this.q = q;
+        this.n = p.multiply(q);
+        this.phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        this.e = e;
+        this.d = Mod.modInverse(e, phiN);
+    }
+
+    private BigInteger chooseE() {
+        BigInteger maybeE = BigInteger.TWO;
+
+        while (!Euclidean.euclidean(maybeE, phiN).equals(BigInteger.ONE)) {
+            maybeE = RandomBigInteger.generateRandomBigInteger(BigInteger.TWO, phiN);
+        }
+
+        return maybeE;
+    }
+
+    private BigInteger generateRandomPrime() {
+        BigInteger maybePrime;
+        BigInteger min = BigInteger.valueOf(1000000000L);
+        BigInteger max = BigInteger.valueOf(100000000000000L);
+
+        while (true) {
+            maybePrime = RandomBigInteger.generateRandomBigInteger(min, max);
+            if (MillerRabin.isPrime(maybePrime, 3)) {
+                return maybePrime;
+            }
+        }
+    }
+
+    public BigInteger encrypt(BigInteger message) {
+        return Mod.modPow(message, e, n);
+    }
+
+    public BigInteger decrypt(BigInteger cipher) {
+        return Mod.modPow(cipher, d, n);
+    }
+
+    public BigInteger decryptCRT(BigInteger cipher) {
+        List<BigInteger> c = createReducedCList(cipher);
+        List<BigInteger> m = createReducedMList();
+
+        return ChineseRemainderTheorem.crt(c, m);
+    }
+
+    private List<BigInteger> createReducedCList(BigInteger cipher) {
+
+        BigInteger reducedC1 = cipher.mod(p);
+        BigInteger reducedC2 = cipher.mod(q);
+
+        BigInteger reducedD1 = d.mod(p.subtract(BigInteger.ONE));
+        BigInteger reducedD2 = d.mod(q.subtract(BigInteger.ONE));
+
+        List<BigInteger> c = new ArrayList<>();
+        c.add(Mod.modPow(reducedC1, reducedD1, p));
+        c.add(Mod.modPow(reducedC2, reducedD2, q));
+
+        return c;
+    }
+
+    private List<BigInteger> createReducedMList() {
+        List<BigInteger> m = new ArrayList<>();
+        m.add(p);
+        m.add(q);
+
+        return m;
+    }
+}
